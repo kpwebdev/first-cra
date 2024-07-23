8cdf79ddbd00281984533732b1c47ebafa75e203
KP Web Dev <kpwebdevishere@gmail.com>
2024-07-23 14:01:43 +0530

    understand how the react works internally

---
+++ src/component/FirstPage.component.jsx
@@ -0,0 +1,14 @@
+// JSX => declarative code
+// Vanilla Javascript => create, select, append, update, replace => imperative code
+
+function FirstPage() {
+  return (
+    <div>
+      <section id="hero">I am hero section</section>
+      <header>Logo</header>
+      <footer>I am the footer</footer>
+    </div>
+  );
+}
+
+export default FirstPage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import FirstPage from "./component/FirstPage.component";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <FirstPage />
+    {(function () {
+      console.dir(FirstPage);
+      console.log("<FirstPage />");
+      console.log(
+        <FirstPage randomProp="I am random Prop" country="India">
+          <h2>I am h2</h2>
+          <h3>I am h3</h3>
+        </FirstPage>
+      );
+
+      // h1
+      const h1 = React.createElement(
+        "h1",
+        {
+          className: "heading-kp",
+        },
+        React.createElement("span", null, "I am another span"),
+        React.createElement("span", null, "I am span")
+      );
+
+      console.log("h1");
+      console.log(h1);
+    })()}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
